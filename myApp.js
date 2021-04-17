5f73c0063da3f60c1d93ab930a49269bfdb58cb9
brahim.jarallah <brahim.jarallah@gmail.com>
2021-04-17 14:55:44 +0100

    settein app for basic node & express

---
+++ myApp.js
@@ -0,0 +1,30 @@
+var express = require("express")
+var app = express()
+var bGround = require("fcc-express-bground")
+
+module.exports = app
+
+// Meet the Node console
+bGround.log("Hello World")
+
+// Start a Working Express Server
+
+// Serve an HTML File
+
+// Serve Static Assets
+
+// Serve JSON on a Specific Route
+
+// Use the .env File
+
+// Implement a Root-Level Request Logger Middleware
+
+// Chain Middleware to Create a Time Server
+
+// Get Route Parameter Input from the Client
+
+// Get Query Parameter Input from the Client
+
+// Use body-parser to Parse POST Requests
+
+// Get Data from POST Requests
